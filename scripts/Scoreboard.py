a7e724d01bbb3e83cf55d3e91b782a366c246ddd
5T4TiC <jpecsi@me.com>
2014-01-08 11:48:34 -0500

    Created menu and scoreboard

---
+++ scripts/Scoreboard.py
@@ -0,0 +1,33 @@
+from bge import logic
+
+scores = []
+
+def display():
+    
+    scene = logic.getCurrentScene()
+    writer = scene.objects["Writer"]
+    
+    rank = 1
+    name = logic.globalDict["initials"]
+    score = 9
+    
+    tup_entry = (rank, name, score)
+    scores.append(tup_entry)
+    scores.sort(key=lambda entry: entry[1])
+    scores.reverse()
+    
+    for entry, i in zip(scores, range(len(scores)):
+        new_writer = scene.addObject(writer, writer)
+        new_writer["Text"] = "{:d}.  {:s} - {:d}".format(i + 1, entry[0], entry[1])
+        
+        new_writer.worldPosition.y -= 1.25 * i
+        new_writer.localScale = writer.localScale
+    
+    
+def setInitials():
+    
+    scene = logic.getCurrentScene()
+    initials = scene.objects["Initials"]
+    
+    logic.globalDict["initials"] = initials["Text"].strip() 
+
